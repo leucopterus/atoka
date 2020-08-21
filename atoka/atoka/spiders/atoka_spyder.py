73d455bfd9d707e5035437c3b8cf05936d961a8d
leucopterus <strelchenkoedgar@gmail.com>
2020-08-21 17:04:03 +0300

    path fix

--- atoka/atoka/spiders/atoka_spyder.py
+++ atoka/atoka/spiders/atoka_spyder.py
@@ -48,7 +48,7 @@ class AtokaSpider(scrapy.Spider):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.file_path = os.path.join(BASE_DIR, 'atoka/spiders/input/by500/input_4th_week.xlsx')
+        self.file_path = os.path.join(BASE_DIR, 'atoka/spiders/input/input_4th_week.xlsx')
         self.max_objects_in_search = 3
 
         self.buffer = {}
